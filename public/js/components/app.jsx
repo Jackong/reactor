1cd66349b94b85d5e02b74460dc0564d107a1789
Jackong <JackongC@gmail.com>
2015-11-19 10:56:55 +0800

    add state

--- public/js/components/app.jsx
+++ public/js/components/app.jsx
@@ -1,10 +1,22 @@
 import React, { PropTypes } from 'react'
 
 class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            content: 'bq'
+        }
+    }
+    onToggle() {
+        this.setState({content: this.state.content === 'bq' ? 'dp' : 'bq'})
+    }
     render () {
         return (
             <div>
-                {this.props.name}
+                <h2>{this.props.name}</h2>
+                <hr/>
+                <p>{this.state.content}</p>
+                <button onClick={e => this.onToggle(e)}>toggle</button>
             </div>
         )
     }
