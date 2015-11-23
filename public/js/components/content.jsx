0c02a09a2bcd04927c304e9a5e5a3eb020078a27
Jackong <JackongC@gmail.com>
2015-11-23 16:11:55 +0800

    add radium for css in js

--- public/js/components/content.jsx
+++ public/js/components/content.jsx
@@ -1,10 +1,11 @@
 import React, { PropTypes } from 'react'
+import Radium from 'radium'
 
 class Content extends React.Component {
     render () {
         return (
             <div>
-                <p>{this.props.content}</p>
+                <p style={[styles.base, styles.other]}>{this.props.content}</p>
             </div>
         )
     }
@@ -14,4 +15,13 @@ Content.propTypes = {
     content: PropTypes.string.isRequired
 }
 
-export default Content
+const styles = {
+    base: {
+        color: 'red'
+    },
+    other: {
+        fontSize: 30
+    }
+}
+
+export default Radium(Content)
